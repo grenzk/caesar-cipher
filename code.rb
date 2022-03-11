26204f7fc7148fbb98e1f9b315c7b2dad48b5c88
grenzk <74033991+grenzk@users.noreply.github.com>
2022-03-11 13:52:45 +0800

    Add code

--- code.rb
+++ code.rb
@@ -1,3 +1,23 @@
-def caesar_cipher(word, number_of_position)
-  
+require 'pry-byebug'
+
+def caesar_cipher(text, shift_factor)
+  characters = text.chars
+  ciphered_letters = []
+  # binding.pry
+  characters.each_index do |index|
+    character = characters[index]
+    case character
+    when 'a'..'z'
+      letters = (character..'z').to_a + ('a'..'z').to_a
+      ciphered_letters << letters[shift_factor]
+    when 'A'..'Z'
+      letters = (character..'Z').to_a + ('A'..'Z').to_a
+      ciphered_letters << letters[shift_factor]
+    else
+      ciphered_letters << character
+    end
+  end
+  ciphered_letters.join
 end
+
+puts caesar_cipher('What a string!', 5)
